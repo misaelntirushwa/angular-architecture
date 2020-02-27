ecdbd08f0df27c75433e24a328eb2ee8c8c581a3
Misael Ntirushwa <misaelntirushwa@gmail.com>
2020-02-27 09:12:28 -0500

    added home and admin feature modules with lazy loading

--- projects/main-app/src/app/app-routing.module.ts
+++ projects/main-app/src/app/app-routing.module.ts
@@ -1,11 +1,30 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'home' // navigate to home if there was no route
+  },
+  {
+    path: 'home',
+    loadChildren: () =>
+      import('./features/home/home.module').then(m => m.HomeModule)
+  },
+  {
+    path: 'admin',
+    loadChildren: () =>
+      import('./features/admin/admin.module').then(m => m.AdminModule)
+  },
+  {
+    path: '**', // catch all route
+    redirectTo: 'home' // can redirect home or to dedicated "not found" route
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
